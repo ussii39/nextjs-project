1b5eb061a3564831c3375e27d16faca2dc152e5e
ussii39 <usimarutetunori3@outlook.jp>
2021-08-08 21:49:23 +0900

    first-commit

---
+++ pages/blog-page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Blog = () => {
+  return <div>ブログページ</div>;
+};
+
+export default Blog;
---
+++ pages/contact-page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <div>コンタクトページ</div>;
+};
+
+export default Contact;
